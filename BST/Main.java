aef5e05243a3d02632b6b25f2f2fc2778097d46f
WedithaV <wedithav@gmail.com>
2025-06-18 12:17:21 +0530

    BST Updated

--- BST/Main.java
+++ BST/Main.java
@@ -1,130 +1,267 @@
 package BST;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(5);
-        tree.insert(8);
-        tree.insert(7);
-        System.out.println(tree.search(8));
-        tree.delete(8);
-        System.out.println(tree.search(8));
+        BinarySearchTree obj = new BinarySearchTree();
+        obj.insert(45);
+        obj.insert(90);
+        obj.insert(6);
+
+        System.out.println(obj.search(78));
+        System.out.println(obj.search(90));
+
+        System.out.println("Pre Order Traversal");
+        obj.preOrder();
+        obj.delete(6);
+        System.out.println();
+        System.out.println("Post Order Traversal");
+
+        obj.postOrder();
+        System.out.println();
+        System.out.println("In Order Traversal");
+
+        obj.inOrder();
+        System.out.println();
+        System.out.println("Level Order Traversal");
+
+        obj.levelOrder(obj.root);
     }
 }
 
-class Node {
+class TreeNode {
     int key;
-    Node left;
-    Node right;
+    TreeNode left, right;
 
-    public Node(int data) {
-        key = data;
+    public TreeNode(int key) {
+        this.key = key;
         left = right = null;
     }
 }
 
 class BinarySearchTree {
-    Node root;
+    TreeNode root;
 
     public BinarySearchTree() {
         root = null;
     }
 
-    public void insert(int data) {
-        root = insertRec(root, data);
+    // Insertion
+    public void insert(int key) {
+        root = insertRec(root, key);
     }
 
-    private Node insertRec(Node root, int data) {
+    public TreeNode insertRec(TreeNode root, int key) {
         if (root == null) {
-            root = new Node(data);
+            root = new TreeNode(key);
             return root;
         }
-
-        if (data <= root.key) {
-            root.left = insertRec(root.left, data);
-        } else if (data > root.key) {
-            root.right = insertRec(root.right, data);
+        if (root.key > key) {
+            root.left = insertRec(root.left, key);
+        } else if (root.key < key) {
+            root.right = insertRec(root.right, key);
         }
-
         return root;
     }
 
-    public boolean search(int value) {
-        if (searchRec(root, value) == null) {
-            return false;
-        } else {
+    // Search
+    public boolean search(int key) {
+        if (searchRec(root, key) != null) {
             return true;
+        } else {
+            return false;
         }
     }
 
-    private Node searchRec(Node tempNode, int value) {
-        if (tempNode == null || tempNode.key == value) {
-            return tempNode;
+    public TreeNode searchRec(TreeNode root, int key) {
+        if (root == null || root.key == key) {
+            return root;
         }
-        if (value < tempNode.key) {
-            return searchRec(tempNode.left, value);
-        } else if (value > tempNode.key) {
-            return searchRec(tempNode.right, value);
+        if (root.key > key) {
+            return searchRec(root.left, key);
         }
-        return null; // To avoid compiler warnings
+        return searchRec(root.right, key);
     }
 
-    public void delete(int value) {
-        deleteNodeWithTwoChilds(root, value);
+    // Deletion
+
+    // Delete A leaf node
+    public void deleteLeafNode(int key) {
+        root = deleteLeafNodeRec(root, key);
     }
 
-    private Node deleteLeafNode(Node tempnode, int value) {
-        if (tempnode == null) {
+    public TreeNode deleteLeafNodeRec(TreeNode root, int key) {
+        if (root == null) {
             return null;
         }
-        if (value < tempnode.key) {
-            tempnode.left = deleteLeafNode(tempnode.left, value);
-        } else if (value > tempnode.key) {
-            tempnode.right = deleteLeafNode(tempnode.right, value);
-        } else if (value == tempnode.key) {
-            tempnode = null;
-        }
-        return tempnode;
-    }
-
-    private Node deleteNodeWithOneChild(Node tempnode, int value) {
-        if (tempnode == null) {
-            return null;
-        }
-        if (value < tempnode.key) {
-            tempnode.left = deleteNodeWithOneChild(tempnode.left, value);
-        } else if (value > tempnode.key) {
-            tempnode.right = deleteNodeWithOneChild(tempnode.right, value);
-        } else if (value == tempnode.key) {
-            if (tempnode.left == null) {
-                return tempnode.right;
-            } else if (tempnode.right == null) {
-                return tempnode.left;
+        if (root.key > key) {
+            root.left = deleteLeafNodeRec(root.left, key);
+        } else if (root.key < key) {
+            root.right = deleteLeafNodeRec(root.right, key);
+        } else {
+            if (root.right == null && root.left == null) {
+                return null;
+            } else {
+                return root;
             }
         }
-        return tempnode;
+        return root;
     }
 
-    private Node deleteNodeWithTwoChilds(Node tempnode, int value) {
-        if (tempnode == null) {
+    // Delete node with one child
+    public void deleteNodeWithOneChild(int key) {
+        root = deleteNodeWithOneChildRec(root, key);
+    }
+
+    public TreeNode deleteNodeWithOneChildRec(TreeNode root, int key) {
+        if (root == null) {
             return null;
         }
-        if (value < tempnode.key) {
-            tempnode.left = deleteNodeWithTwoChilds(tempnode.left, value);
-        } else if (value > tempnode.key) {
-            tempnode.right = deleteNodeWithTwoChilds(tempnode.right, value);
-        } else if (value == tempnode.key) {
-            Node successor = findSuccessor(tempnode.right);
-            tempnode.key = successor.key;
-            tempnode.right = deleteNodeWithTwoChilds(tempnode.right, successor.key);
+        if (root.key > key) {
+            root.left = deleteNodeWithOneChildRec(root.left, key);
+        } else if (root.key < key) {
+            root.right = deleteNodeWithOneChildRec(root.right, key);
+        } else {
+            if (root.right == null) {
+                return root.left;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
         }
-        return tempnode;
+        return root;
     }
 
-    public Node findSuccessor(Node node) {
-        while (node.left != null) {
-            node = node.left;
+    // Delete Node with two Children
+    public void deleteNodeWithTwoChildren(int key) {
+        root = deleteNodeWithTwoChildrenRec(root, key);
+    }
+
+    public TreeNode deleteNodeWithTwoChildrenRec(TreeNode root, int key) {
+        if (root == null) {
+            return null;
         }
-        return node;
+        if (root.key > key) {
+            root.left = deleteNodeWithTwoChildrenRec(root.left, key);
+        } else if (root.key < key) {
+            root.right = deleteNodeWithTwoChildrenRec(root.right, key);
+        } else {
+            TreeNode temp = findSuccessor(root.right);
+            root.key = temp.key;
+            root.right = deleteNodeWithTwoChildrenRec(root.right, temp.key);
+        }
+        return root;
+    }
+
+    // All Delete methods in one function
+    public void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    public TreeNode deleteRec(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.key > key) {
+            root.left = deleteRec(root.left, key);
+        } else if (root.key < key) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            if (root.right == null && root.left == null) {
+                return null;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode node = findPredeccessor(root.left);
+            root.key = node.key;
+            root.left = deleteRec(root.left, node.key);
+        }
+        return root;
+    }
+
+    // Find Successor
+    public TreeNode findSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    // Find predeccessor
+    public TreeNode findPredeccessor(TreeNode root) {
+        while (root.right != null) {
+            root = root.right;
+        }
+        return root;
+    }
+
+    // Traversal
+
+    // Pre order Traversal
+    public void preOrder() {
+        preOrderRec(root);
+    }
+
+    public void preOrderRec(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preOrderRec(root.left);
+            preOrderRec(root.right);
+        }
+    }
+
+    // In order Traversal
+    public void inOrder() {
+        inOrderRec(root);
+    }
+
+    public void inOrderRec(TreeNode root) {
+        if (root != null) {
+            inOrderRec(root.left);
+            System.out.print(root.key + " ");
+            inOrderRec(root.right);
+        }
+    }
+
+    // Post order Traversal
+    public void postOrder() {
+        postOrderRec(root);
+    }
+
+    public void postOrderRec(TreeNode root) {
+        if (root != null) {
+            postOrderRec(root.left);
+            postOrderRec(root.right);
+            System.out.print(root.key + " ");
+        }
+    }
+
+    // Breadth First search
+
+    // Level Order traversal
+    public void levelOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.key + " ");
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
     }
 }
